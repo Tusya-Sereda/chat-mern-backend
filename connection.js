c2d2b1ae625465b902f80b608c21d7dceccd133c
Natalia Sereda <natalia.sereda.ex@gmail.com>
2022-08-02 14:12:16 +0300

    change parametrs in mongoose

--- connection.js
+++ connection.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(`${process.env.MONGO_DB}`, () => {
-  console.log("Connected to MongoDB");
-});
+mongoose.connect(
+  `${process.env.MONGO_DB}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
